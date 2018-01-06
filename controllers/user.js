608a806a09e826cdf58d8a22bf21b6e9bec3ba4c
Felipe Orlando <fobsouza@gmail.com>
2018-01-06 00:30:47 -0200

    Add user list and crud

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,54 @@
-class User {
+import User from '../models/user';
+
+class UserController {
   index(req, res) {
-    res.status(200).send('It work!');
+    User.find({}, (error, users) => {
+      if (error) res.status(404).json({ error });
+
+      res.status(200).json({ users });
+    });
   }
 
   create(req, res) {
-    res.status(200).send('It work!');
+    const params = req.body.user;
+    const newUser = new User(params);
+
+    newUser.save((error, user) => {
+      if (error) return res.status(200).json({ error });
+
+      res.status(200).json({ user });
+    });
   }
 
   show(req, res) {
-    res.status(200).send('It work!');
+    User.findById(req.params.id, (error, user) => {
+      if (error) res.status(404).json({ error });
+
+      res.status(200).json({ user });
+    });
   }
 
   update(req, res) {
-    res.status(200).send('It work!');
+    const params = req.body.user;
+    const userId = req.params.id;
+
+    User.findByIdAndUpdate(userId, { $set: params }, { new: true }, (error, user) => {
+      if (error) return res.status(200).json({ error });
+
+      res.status(200).json({ user });
+    });
   }
 
   delete(req, res) {
-    res.status(200).send('It work!');
+    User.findOneAndRemove(req.params.id, (error, user) => {
+      if (error) res.status(404).json({ error });
+
+      res.status(200).json({
+        message: 'User was deleted',
+        userId: user._id,
+      });
+    });
   }
 }
 
-export default User;
+export default UserController;
