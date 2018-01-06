73962313a2b988ba7f0040bbe1e3234ce24cb8e4
Felipe Orlando <fobsouza@gmail.com>
2018-01-06 00:06:14 -0200

    Remove commented code

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,3 @@
-// import express from 'express';
-// import UserController from '../controllers/user';
-
-// const router = express.Router();
-// const userController = new UserController();
-
-// router
-//   .get('/', userController.index)
-//   .post('/', userController.create)
-//   .get('/:id', userController.show)
-//   .put('/:id', userController.update)
-//   .delete('/:id', userController.delete);
-
-// export default { init: router };
-
-
 import express from 'express';
 import UserController from '../controllers/user';
 
