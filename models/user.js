f9a6075e40a7561d4b2820c651bc72b565814759
Felipe Orlando <fobsouza@gmail.com>
2018-01-06 13:25:59 -0200

    Configure nodemon, change bcrypt module and refact User methods

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt-nodejs';
 import UserSchema from './schemas/user';
 
 class User {
   static encryptPassword(password, next) {
     if (this.isNew || this.isModified('password')) {
-      bcrypt.genSalt(10, (error, salt) => {
-        if (error) return next(error);
+      bcrypt.genSalt(10, (saltError, salt) => {
+        if (saltError) return next(saltError);
 
-        bcrypt.hash(user.password, salt, (error, hash) => {
-          if (error) return next(error);
+        bcrypt.hash(this.password, salt, (hashError, hash) => {
+          if (hashError) return next(hashError);
 
           this.password = hash;
           next();
@@ -18,9 +18,9 @@ class User {
     }
   }
 
-  static comparePassword(password, callback) {
-    bcrypt.compare(password, this.password, (err, isMatch) => {
-      if (err) return callback(err);
+  static comparePassword(passwordParam, callback) {
+    bcrypt.compare(passwordParam, this.password, (error, isMatch) => {
+      if (error) return callback(error);
 
       callback(null, isMatch);
     });
