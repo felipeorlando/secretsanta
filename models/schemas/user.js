e3f18552ec4101b4f9b16853477b85b09740af74
Felipe Orlando <fobsouza@gmail.com>
2018-01-07 02:15:00 -0200

    Change user validator to email validator

--- models/schemas/user.js
+++ models/schemas/user.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import UserValidator from '../validators/user';
+import EmailValidator from '../validators/email';
 
 class Schema {
   static init() {
@@ -13,7 +13,7 @@ class Schema {
         lowercase: true,
         unique: true,
         required: true,
-        validate: [UserValidator.validateEmail, 'Please fill a valid email address'],
+        validate: [EmailValidator.format, 'Please fill a valid email address'],
       },
       password: {
         type: String,
--- models/validators/email.js
+++ models/validators/email.js
@@ -1,8 +1,8 @@
-class UserValidator {
-  static validateEmail(email) {
+class EmailValidator {
+  static format(email) {
     const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return emailFormat.test(email);
   }
 }
 
-export default UserValidator;
+export default EmailValidator;
