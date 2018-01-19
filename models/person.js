7d744707ad160d64653198df026dba48cb3c1c5f
Felipe Orlando <fobsouza@gmail.com>
2018-01-19 17:25:20 -0200

    Matches!

--- models/person.js
+++ models/person.js
@@ -1,9 +1,5 @@
 import mongoose from 'mongoose';
-import async from 'async';
-import _ from 'underscore';
 import PersonSchema from './schemas/person';
-
-let allPersons = [];
 
 class Person extends mongoose.Model {
   static async matchAll() {
@@ -11,17 +7,9 @@ class Person extends mongoose.Model {
     const resetedPersons = await Person.resetAll(persons);
     const matcheds = await Person.match(resetedPersons);
 
-
-    console.log('________________________________________________ done!');
-    // async.waterfall([
-    //   (done) => Person.getAll(done),
-    //   (persons, done) => Person.resetAll(persons, done),
-    //   (persons, done) => {
-    //     console.log(persons);
-    //   },
-    // ], (done) => {
-    //   console.log('done!');
-    // });
+    matcheds.map((person) => {
+      // console.log(`${person._id} - ${person.matchedPerson}`);
+    });
   }
 
   static getAll() {
@@ -41,23 +29,26 @@ class Person extends mongoose.Model {
 
   static match(persons) {
     return new Promise((resolve) => {
-      const newPersons = persons;
+      const candidates = persons;
+      const candidatesSize = candidates.length;
 
-      persons.map((person, index) => {
-        const friend = newPersons.filter((candidate) => {
-          return candidate.isMatched === false && candidate._id !== person._id;
-        })[0];
-
-        console.log(friend);
-
-        // console.log(friend);
-        console.log('-----------------');
-
-        // newPersons[index].matchedPerson = friend._id;
+      persons.map((person) => {
+        for (let i = 0; i < candidatesSize; i++) {
+          if (candidates[i]._id !== person._id && candidates[i].matchedPerson !== person._id && candidates[i].isMatched === false) {
+            candidates[i].isMatched = true;
+            person.matchedPerson = candidates[i]._id;
+            console.log(`${person.name} - ${candidates[i].name}`);
+            break;
+          }
+        }
       });
-  
-      resolve(newPersons);
+
+      resolve(persons);
     });
+  }
+
+  static findFriend(person) {
+    //
   }
 }
 
