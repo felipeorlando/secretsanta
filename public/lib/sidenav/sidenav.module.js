d05d474d5b66c6765db9377679d3c03b534624a0
Felipe Orlando <fobsouza@gmail.com>
2018-02-10 05:41:46 -0200

    New client configs

--- public/lib/sidenav/sidenav.module.js
+++
@@ -1,8 +0,0 @@
-import SidenavController from './sidenav.controller';
-
-
-let sidenavModule = angular.module('demo.sidenav', []);
-
-sidenavModule.controller('SidenavController', SidenavController);
-
-export default sidenavModule = sidenavModule.name
