4843051d0edeb8a3a8d6789a449e05cec68ffe22
Felipe Orlando <fobsouza@gmail.com>
2018-02-12 16:21:01 -0200

    Quotation marks to '

--- api/configs/index.js
+++ api/configs/index.js
@@ -1,27 +1,27 @@
 const configs = {
   app: {
-    host: process.env.HOST || "localhost",
+    host: process.env.HOST || 'localhost',
     port: process.env.PORT || 8888,
-    jwtSecret: process.env.JWT_SECRET || "secretOfSecretSanta"
+    jwtSecret: process.env.JWT_SECRET || 'secretOfSecretSanta'
   },
 
   db: {
-    name: process.env.DB_NAME || "secretSanta",
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "root",
-    host: process.env.DB_HOST || "localhost"
+    name: process.env.DB_NAME || 'secretSanta',
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    host: process.env.DB_HOST || 'localhost'
   },
 
   redis: {
-    pkg: process.env.REDIS_PKG || "ioredis",
-    host: process.env.REDIS_HOST || "127.0.0.1",
+    pkg: process.env.REDIS_PKG || 'ioredis',
+    host: process.env.REDIS_HOST || '127.0.0.1',
     password: process.env.REDIS_PASSWORD || null,
     port: process.env.REDIS_PORT || 6379,
     database: process.env.REDIS_DB || 0
   },
 
   mailgun: {
-    username: "api",
+    username: 'api',
     key: process.env.MAILGUN_KEY
   }
 };
