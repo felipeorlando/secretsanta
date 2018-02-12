2bf68471bf39d530df3dec1f3b41dc4c3e9f4450
Felipe Orlando <fobsouza@gmail.com>
2018-02-12 16:19:01 -0200

    Add env variables to config

--- api/configs/index.js
+++ api/configs/index.js
@@ -1,29 +1,29 @@
 const configs = {
   app: {
-    host: 'localhost',
-    port: 8888,
-    jwtSecret: 'secretOfSecretSanta',
+    host: process.env.HOST || "localhost",
+    port: process.env.PORT || 8888,
+    jwtSecret: process.env.JWT_SECRET || "secretOfSecretSanta"
   },
 
   db: {
-    name: 'secretSanta',
-    username: 'root',
-    password: 'root',
-    host: 'localhost',
+    name: process.env.DB_NAME || "secretSanta",
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    host: process.env.DB_HOST || "localhost"
   },
 
   redis: {
-    pkg: 'ioredis',
-    host: '127.0.0.1',
-    password: null,
-    port: 6379,
-    database: 0,
+    pkg: process.env.REDIS_PKG || "ioredis",
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    password: process.env.REDIS_PASSWORD || null,
+    port: process.env.REDIS_PORT || 6379,
+    database: process.env.REDIS_DB || 0
   },
 
   mailgun: {
-    username: 'api',
-    key: 'key-5f2d55ebbd5c877154be4df39be0e4ed',
-  },
+    username: "api",
+    key: process.env.MAILGUN_KEY
+  }
 };
 
 export default configs;
