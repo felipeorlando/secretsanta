03953a83d3d707add1af712400dab72fd532c00b
Felipe Orlando <fobsouza@gmail.com>
2018-02-13 18:07:44 -0200

    Create users edit and create

--- client/src/modules/users.js
+++ client/src/modules/users.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import UsersController from '../controllers/users';
-import listTemplate from '../templates/users.html';
+import listTemplate from "../templates/users.html";
+import newTemplate from '../templates/users.new.html';
 import editTemplate from '../templates/users.edit.html';
 
 let usersModule = angular.module('app.users', []);
@@ -16,6 +17,13 @@ usersModule.config(($stateProvider) => {
       controller: 'UsersController',
       controllerAs: '$usersCtrl',
       template: listTemplate,
+      title: 'Users'
+    })
+    .state('app.usersNew', {
+      url: '/users/new',
+      controller: 'UsersController',
+      controllerAs: '$usersCtrl',
+      template: newTemplate,
       title: 'Users'
     })
     .state('app.usersEdit', {
