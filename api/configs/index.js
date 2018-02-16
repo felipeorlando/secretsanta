28e765af317d39a2f430838768203de609ba6485
Felipe Orlando <fobsouza@gmail.com>
2018-02-16 13:00:23 -0200

    Update database mongo config

--- api/configs/index.js
+++ api/configs/index.js
@@ -2,14 +2,11 @@ const configs = {
   app: {
     host: process.env.HOST || 'localhost',
     port: process.env.PORT || 8888,
-    jwtSecret: process.env.JWT_SECRET || 'secretOfSecretSanta'
+    jwtSecret: process.env.JWT_SECRET || 'secretOfSecretSanta',
   },
 
   db: {
-    name: process.env.DB_NAME || 'secretSanta',
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    host: process.env.DB_HOST || 'localhost'
+    config: process.env.MONGO_CONFIG || 'mongodb://localhost/secretsanta',
   },
 
   redis: {
@@ -17,13 +14,13 @@ const configs = {
     host: process.env.REDIS_HOST || '127.0.0.1',
     password: process.env.REDIS_PASSWORD || null,
     port: process.env.REDIS_PORT || 6379,
-    database: process.env.REDIS_DB || 0
+    database: process.env.REDIS_DB || 0,
   },
 
   mailgun: {
     username: 'api',
-    key: process.env.MAILGUN_KEY
-  }
+    key: process.env.MAILGUN_KEY,
+  },
 };
 
 export default configs;
